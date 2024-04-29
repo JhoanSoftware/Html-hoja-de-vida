7ee22465deb72cf4e44feb1fa6738fd06cd98de2
JhoanSoftware <148224877+JhoanSoftware@users.noreply.github.com>
2024-04-29 11:32:27 -0500

    Add files via upload

---
+++ evaluacion/jhoan.js
@@ -0,0 +1,19 @@
+//  let num1 = prompt("ingrese el primer numero");
+//  let num2 = prompt("ingrese el segundo numero");
+
+//  if(num1===num2){
+//     alert(`los 2 numeros que ingreso son iguales`);
+//  }
+
+//  else if(num1>num2){
+//     alert (`De los 2 numeros que ingreso el numero ${num1} es menor `);
+//  }
+//  else{
+//     alert (`De los 2 numeros que ingreso el numero ${num2} es mayor`);
+//  }
+  
+
+//  let nombre = prompt("ingrese su nombre");
+//  let edad= (prompt("ingrese su edad"));
+
+//  edad<18? alert (`hola ${nombre}, eres menor de edad, tienes ${edad}`) : alert (`hola ${nombre}, eres mayor de edad, tienes ${edad}`);
